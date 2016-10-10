28e4ff83319d630ccac8d6441cd1aef0076d191d
matheswaaran <matheswaarans@gmail.com>
2016-10-10 11:49:12 +0530

    db file

--- ConfigDB.sql
+++ ConfigDB.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Sep 29, 2016 at 04:10 AM
--- Server version: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Host: 127.0.0.1
+-- Generation Time: Oct 10, 2016 at 08:17 AM
+-- Server version: 10.1.8-MariaDB
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
